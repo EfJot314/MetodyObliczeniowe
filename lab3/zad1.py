bf0751a9a0dbc0ab7bcac481a929615756ae71c6
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-03-18 19:06:37 +0100

    polowa podpunktu c)

--- lab3/zad1.py
+++ lab3/zad1.py
@@ -24,10 +24,57 @@ yData = np.array([
                 ])
 
 
-vandermond_matrix = np.zeros((N,N))
+#funcje bazowe
+fi1 = lambda t: t
+fi2 = lambda t: t - 1900
+fi3 = lambda t: t - 1940
+fi4 = lambda t: (t - 1940) / 40
 
 
-print(vandermond_matrix)
+#macierze
+M1 = np.zeros((N,N))
+M2 = np.zeros((N,N))
+M3 = np.zeros((N,N))
+M4 = np.zeros((N,N))
+
+
+
+#uzupelnianie macierzy
+for i in range(N):
+    for j in range(N):
+        M1[i,j] = (fi1(xData[i]))**j
+        M2[i,j] = (fi2(xData[i]))**j
+        M3[i,j] = (fi3(xData[i]))**j
+        M4[i,j] = (fi4(xData[i]))**j
+
+
+#wspolczynniki uwarunkowania
+cond1 = np.linalg.cond(M1)
+cond2 = np.linalg.cond(M2)
+cond3 = np.linalg.cond(M3)
+cond4 = np.linalg.cond(M4)
+
+print("Wspolczynniki uwarunkowania:", cond1, cond2, cond3, cond4)
+
+
+#wybieranie najlepszego uwarunkowania
+M = np.zeros((N,N))
+minCond = min(cond1, cond2, cond3, cond4)
+print("Najmniejszy wspolczynnik uwarunkowania:", minCond)
+if(cond1 == minCond):
+    M = M1
+elif(cond2 == minCond):
+    M = M2
+elif(cond3 == minCond):
+    M = M3
+elif(cond4 == minCond):
+    M = M4
+
+
+#wyznaczanie wspolczynnikow
+C = np.linalg.solve(M, yData)
+
+print("Otrzymane wspolczynniki:", C)
 
 
 
