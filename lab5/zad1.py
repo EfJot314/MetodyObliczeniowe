f66ac513255f7589961123e34db499d35cc3628e
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-03-28 15:33:51 +0200

    rozpoczecie lab5

---
+++ lab5/zad1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#dane dotyczace populacji
+xData = np.array([
+                1900, 1910, 1920,
+                1930, 1940, 1950,
+                1960, 1970, 1980
+                ])
+
+yData = np.array([
+                76212168,
+                92228496,
+                106021537,
+                123202624,
+                132164569,
+                151325798,
+                179323175,
+                203302031,
+                226542199
+                ])
+
+
+
+
+
+
+
