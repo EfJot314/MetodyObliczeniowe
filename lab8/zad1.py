8cbdda7ce8afedffcd8eb4c44fce35bcc97b5369
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-05-09 09:16:56 +0200

    schematy iteracyjne - poczatek

--- lab8/zad1.py
+++ lab8/zad1.py
@@ -2,3 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#funkcje dane w zadaniu
+f = lambda x: x*x - 3*x + 2
+
+g1 = lambda x: (x*x + 2) / 3
+g2 = lambda x: (3*x - 2) ** 0.5
+g3 = lambda x: 3 - 2/x
+g4 = lambda x: (x*x - 2) / (2*x - 3)
+
